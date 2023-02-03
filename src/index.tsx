ff6ebd87f4b2845a8436dacf9896da347bdfa2ad
extrahash <ExtraHash@protonmail.com>
2023-02-03 14:07:19 -0700

    verification

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,69 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import "@rainbow-me/rainbowkit/styles.css";
+import {
+    getDefaultWallets,
+    RainbowKitProvider,
+    midnightTheme,
+} from "@rainbow-me/rainbowkit";
+import { Chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
+
+const cantoChain: Chain = {
+    id: 7700,
+    name: "Canto",
+    network: "canto",
+    nativeCurrency: {
+        decimals: 18,
+        name: "Canto",
+        symbol: "CANTO",
+    },
+    rpcUrls: {
+        default: {
+            http: ["https://canto.slingshot.finance"],
+        },
+    },
+    blockExplorers: {
+        default: {
+            name: "Canto Explorer",
+            url: "https://evm.explorer.canto.io",
+        },
+    },
+    testnet: false,
+};
+
+const { chains, provider } = configureChains([cantoChain], [publicProvider()]);
+
+const { connectors } = getDefaultWallets({
+    appName: "My RainbowKit App",
+    chains,
+});
+
+const wagmiClient = createClient({
+    autoConnect: true,
+    connectors,
+    provider,
+});
+
 root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </React.StrictMode>
+    <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider
+            chains={chains}
+            theme={midnightTheme({
+                accentColor: "#000",
+                fontStack: "system",
+            })}
+        >
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </RainbowKitProvider>
+    </WagmiConfig>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/Verify.tsx
+++ src/pages/Verify.tsx
@@ -1,10 +1,60 @@
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import bg from "../assets/bg.png";
-import welcome from "../assets/eye.png";
-import altoButton from "../assets/alto.png";
-import twitterButton from "../assets/twitter.png";
+import eye from "../assets/eye.png";
+import eye2 from "../assets/eye2.png";
+import eye3 from "../assets/eye3.png";
+
+import { useEffect, useState } from "react";
+import { ConnectButton, useConnectModal } from "@rainbow-me/rainbowkit";
+import { useAccount, useSignMessage } from "wagmi";
+
+interface DiscordUser {
+    id: string;
+    username: string;
+    discriminator: string;
+    avatar: string;
+    verified: boolean;
+    email: string;
+    flags: number;
+    banner: string;
+    accent_color: number;
+    premium_type: number;
+    public_flags: number;
+}
 
 export function Verify() {
+    const fragment = new URLSearchParams(window.location.hash.slice(1));
+    const [accessToken, tokenType] = [
+        fragment.get("access_token"),
+        fragment.get("token_type"),
+    ];
+    const [user, setUser] = useState<DiscordUser | null>(null);
+
+    const { address, isConnecting, isDisconnected } = useAccount();
+    const { openConnectModal } = useConnectModal();
+    const { signMessageAsync } = useSignMessage();
+
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        if (!accessToken || !tokenType) {
+            return;
+        }
+
+        const grabDetails = async () => {
+            const res = await fetch("https://discord.com/api/users/@me", {
+                headers: {
+                    authorization: `${tokenType} ${accessToken}`,
+                },
+            });
+            const user = await res.json();
+            console.log(user);
+            setUser(user);
+        };
+
+        grabDetails();
+    }, [accessToken]);
+
     return (
         <div
             style={{
@@ -17,6 +67,16 @@ export function Verify() {
             }}
             className="flex flex-col items-centered"
         >
+            <div
+                style={{
+                    zIndex: 1005,
+                    position: "absolute",
+                    right: "1rem",
+                    top: "1rem",
+                }}
+            >
+                <ConnectButton />
+            </div>
             <div>
                 <img
                     style={{
@@ -26,11 +86,161 @@ export function Verify() {
                         margin: "auto",
                         marginTop: "12%",
                     }}
-                    src={welcome}
+                    src={eye}
                     alt="welcome to frenly faces"
                 ></img>
             </div>
-            <div className="">
+            {!success && !accessToken && (
+                <div className="">
+                    <div
+                        className="mt-20 mx-auto text-center"
+                        style={{ zIndex: 1000 }}
+                    >
+                        <p
+                            className="text-center mx-auto"
+                            style={{
+                                textAlign: "center",
+                                fontSize: 24,
+                                fontFamily: "monospace",
+                                marginBottom: "2rem",
+                            }}
+                        >
+                            please{" "}
+                            <button
+                                onClick={() => {
+                                    window.location.href = `https://discord.com/api/oauth2/authorize?client_id=1071113835659923466&redirect_uri=${encodeURIComponent(
+                                        process.env
+                                            .REACT_APP_DISCORD_REDIRECT_URI!
+                                    )}&response_type=token&scope=identify`;
+                                }}
+                                style={{
+                                    textDecoration: "underline",
+                                    color: "#3e35f9",
+                                }}
+                            >
+                                verify your Discord
+                            </button>{" "}
+                            to be Seen
+                        </p>
+                    </div>
+                </div>
+            )}
+            {!success && accessToken && user && (
+                <div className="">
+                    <div
+                        className="mt-20 mx-auto text-center"
+                        style={{ zIndex: 1000 }}
+                    >
+                        <p
+                            className="text-center mx-auto"
+                            style={{
+                                textAlign: "center",
+                                fontSize: 24,
+                                fontFamily: "monospace",
+                                marginBottom: "2rem",
+                            }}
+                        >
+                            <p>
+                                we See you{" "}
+                                <strong>
+                                    {user.username}#{user.discriminator}
+                                </strong>
+                            </p>
+
+                            {isDisconnected && (
+                                <p style={{ marginTop: "1rem" }}>
+                                    please{" "}
+                                    <button
+                                        onClick={openConnectModal}
+                                        style={{
+                                            textDecoration: "underline",
+                                            color: "#3e35f9",
+                                        }}
+                                    >
+                                        connect your wallet
+                                    </button>{" "}
+                                    to continue
+                                </p>
+                            )}
+                            {!isDisconnected && address && (
+                                <p style={{ marginTop: "1rem" }}>
+                                    please{" "}
+                                    <button
+                                        onClick={async () => {
+                                            const res = await fetch(
+                                                `https://api.frenlyfaces.xyz/verify/token`,
+                                                {
+                                                    method: "POST",
+                                                    body: JSON.stringify({
+                                                        address,
+                                                    }),
+                                                    headers: {
+                                                        "Content-Type":
+                                                            "application/json",
+                                                    },
+                                                }
+                                            );
+
+                                            if (res.status === 200) {
+                                                const {
+                                                    token,
+                                                }: { token: string } =
+                                                    await res.json();
+
+                                                const result =
+                                                    await signMessageAsync({
+                                                        message: token,
+                                                    });
+
+                                                const payload = {
+                                                    message: token,
+                                                    signature: result,
+                                                    discordToken: accessToken,
+                                                    discordTokenType: tokenType,
+                                                };
+
+                                                console.log(payload);
+
+                                                const verifyRes = await fetch(
+                                                    `https://api.frenlyfaces.xyz/verify/verify`,
+                                                    {
+                                                        method: "POST",
+                                                        body: JSON.stringify(
+                                                            payload
+                                                        ),
+                                                        headers: {
+                                                            "Content-Type":
+                                                                "application/json",
+                                                        },
+                                                    }
+                                                );
+
+                                                if (verifyRes.status === 200) {
+                                                    setSuccess(true);
+                                                } else {
+                                                    alert(
+                                                        "We couldn't verify you as a frenly face. Please make sure you have the correct wallet connected. If you continue to experience problems, DM us on Twitter @FrenlyFaces"
+                                                    );
+                                                }
+                                            } else {
+                                                alert(res.statusText);
+                                            }
+                                        }}
+                                        style={{
+                                            textDecoration: "underline",
+                                            color: "#3e35f9",
+                                        }}
+                                    >
+                                        sign a message
+                                    </button>{" "}
+                                    to verify your membership
+                                </p>
+                            )}
+                        </p>
+                    </div>
+                </div>
+            )}
+            {success && (
                 <div
                     className="mt-20 mx-auto text-center"
                     style={{ zIndex: 1000 }}
@@ -44,24 +254,21 @@ export function Verify() {
                             marginBottom: "2rem",
                         }}
                     >
-                        Please{" "}
-                        <button
-                            onClick={() => {
-                                window.location.href = `https://discord.com/api/oauth2/authorize?client_id=1071113835659923466&redirect_uri=${encodeURIComponent(
-                                    process.env.REACT_APP_DISCORD_REDIRECT_URI!
-                                )}&response_type=token&scope=identify`;
-                            }}
-                            style={{
-                                textDecoration: "underline",
-                                color: "#3e35f9",
-                            }}
-                        >
-                            verify your Discord
-                        </button>{" "}
-                        to be Seen.
+                        you have been Seen
+                    </p>
+                    <p
+                        className="text-center mx-auto"
+                        style={{
+                            textAlign: "center",
+                            fontSize: 24,
+                            fontFamily: "monospace",
+                            marginBottom: "2rem",
+                        }}
+                    >
+                        you may return to discord
                     </p>
                 </div>
-            </div>
+            )}
         </div>
     );
 }
